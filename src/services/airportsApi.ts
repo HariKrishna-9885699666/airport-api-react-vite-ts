80b7c878ce7ef88d4b151bad8777f5df2a0d97b7
Hari Krishna Anem <anemharikrishna@gmail.com>
2024-05-28 17:06:57 +0530

    Update airportsApi.ts

--- src/services/airportsApi.ts
+++ src/services/airportsApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AirportDef } from "../types";
 
 const API_URL = "https://api.api-ninjas.com/v1/airports";
-const API_KEY = "tOK4dDb0/maoKPjdBBOZeA==9Un1kJzyclB50MdY"; // Replace with your actual API key
+const API_KEY = process.env.REACT_APP_API_NINJA_KEY;
 
 export async function fetchAirports(
   nameQuery: string,
